9b09515202321077996e1d977ceada1248a25a29
rohmatsb <rohmatsb@gmail.com>
2025-03-25 11:59:57 +0700

    update setup.go

--- setup.go
+++ setup.go
@@ -1,25 +1,14 @@
 package main
 import "fmt"
 
-func archtype() {
-
-    // Print pesan untuk memilih arsitektur
-    fmt.Println("Masukkan arsitektur :")
-    fmt.Println("Contoh 'amd64' atau 'arm64'")
-
-    // Mendefinisikan variabel dan scan value arch
-    var arch string
-    fmt.Scanln(&arch)
-
-    if arch == "amd64" {
-    fmt.Println("Arsitektur kamu amd64")
-    } else if arch == "arm64" {
-    fmt.Println("Arsitektur kamu arm64")
-    } else {
-    fmt.Println("Format salah, masukkan sesuai contoh!")
-    }
+func welcome() {
+  fmt.Println("============")
+  fmt.Println("Hugo Blog Manager")
+  fmt.Println("============")
+  fmt.Println("")
+  fmt.Println("Welcome to easier hugo blog management. Worry less, and focus on your writing.")
 }
 
 func main() {
-    archtype()
+  welcome()
 }
