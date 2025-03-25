01c8321843282b1cc520faee227d6d3e39bc5b87
rohmatsb <rohmatsb@gmail.com>
2025-03-25 13:13:07 +0700

    update

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Print pesan welcome
+echo "=================="
+echo "Hugo Sites Manager"
+echo "=================="
+echo ""
+echo "Welcome to easier hugo sites management. Worry less, and focus on your writings."
+
+# Confirmation message to continue
+read -p "Press 'enter' to continue..."
+
