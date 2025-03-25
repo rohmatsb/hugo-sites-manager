b215cdf40937a302b18d06dc56e075cce2a49eaa
Rohmat Subekti <rohmatsb@gmail.com>
2025-03-25 05:20:30 +0000

    fungsi welcome

--- setup.go
+++ setup.go
@@ -2,11 +2,17 @@ package main
 import "fmt"
 
 func welcome() {
-  fmt.Println("============")
+  // Print pesan welcome
+  fmt.Println("=================")
   fmt.Println("Hugo Blog Manager")
-  fmt.Println("============")
+  fmt.Println("=================")
   fmt.Println("")
   fmt.Println("Welcome to easier hugo blog management. Worry less, and focus on your writing.")
+
+  // Confirmation
+  fmt.Print("Press 'enter' to continue...")
+  var tekan_enter string
+  fmt.Scanln(&tekan_enter)
 }
 
 func main() {
